0bfadddc228836183234a2207ac712d48765027d
Takuya Shizukuishi <sititou70@gmail.com>
2021-06-06 11:56:39 +0900

    feat: correspond to multiple result

---
+++ src/index/sqrt3.ts
@@ -0,0 +1,31 @@
+import { FloatToStr } from '../float/convert';
+import {
+  RemoveExtraZerosFloat,
+  ShrinkFloatExponentOneDigit,
+} from '../float/utils';
+import { NewtonSqrtStep } from '../sqrt/sqrt';
+import { NumberToNatural } from '../utils/natural_number';
+
+type squared_value = {
+  fraction: [1, 1];
+  exponent: NumberToNatural<0>;
+  is_negative: false;
+};
+type initial_value = {
+  fraction: [0, 0, 1, 0, 1];
+  exponent: NumberToNatural<1>;
+  is_negative: false;
+};
+
+type result1 = NewtonSqrtStep<initial_value, squared_value>;
+type result2 = NewtonSqrtStep<result1, squared_value>;
+
+type result3_tmp1 = NewtonSqrtStep<result2, squared_value>;
+type result3_tmp2 = ShrinkFloatExponentOneDigit<result3_tmp1>;
+type result3_tmp3 = ShrinkFloatExponentOneDigit<result3_tmp2>;
+type result3 = RemoveExtraZerosFloat<result3_tmp3>;
+
+type result4_tmp1 = NewtonSqrtStep<result3, squared_value>;
+type result4 = RemoveExtraZerosFloat<result4_tmp1>;
+
+const result: FloatToStr<result4> = null;
